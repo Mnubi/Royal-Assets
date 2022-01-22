25d3543bed6c6f5e98d0ac91ef8daef7fc94e326
Daniel6996-arch <nyutu.daniel@student.moringaschool.com>
2022-01-22 16:59:17 +0300

    add initial user-profile component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './profile/profile.component';
+import { UserprofileComponent } from './userprofile/userprofile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent,
+    UserprofileComponent
   ],
   imports: [
     BrowserModule,
