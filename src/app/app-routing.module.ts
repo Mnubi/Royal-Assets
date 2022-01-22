eb5900dfcf444a40f09e2c208727edde1f0f3ff3
Alikutepa <juliana.alikutepa@student.moringaschool.com>
2022-01-22 18:01:48 +0300

    add image in registration page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { RegistrationComponent } from './registration/registration.component';
 
 const routes: Routes = [
   {path:'',component:HomeComponent},
+  {path:'registration',component:RegistrationComponent}
 ];
 
 @NgModule({
