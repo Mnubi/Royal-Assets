3df179fc8ed29d858cc9cf081ab2e9891936b3d7
Alikutepa <juliana.alikutepa@student.moringaschool.com>
2022-01-23 13:04:29 +0300

    update changes

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import {FormGroup, FormBuilder} from "@angular/forms"
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-registration',
@@ -7,9 +10,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationComponent implements OnInit {
 
-  constructor() { }
+  public signupForm !: FormGroup
+
+  constructor(private formBuilder : FormBuilder, private http : HttpClient, private router:Router) { }
 
   ngOnInit(): void {
+    this.signupForm = this.formBuilder.group({
+      fullname:[''],
+      email:[''],
+      role:[''],
+      password:[''],
+      confirmPasword:['']
+    })
   }
+  signUp(){
+    this.http.post<any>("/registration",this.signupForm.value)
+    .subscribe(res=>{
+      alert("Signup successful");
+      this.signupForm.reset();
+      this.router.navigate(['login'])
+    },err=>{
+      alert("Something went wrong")
+    })
 
+}
 }
