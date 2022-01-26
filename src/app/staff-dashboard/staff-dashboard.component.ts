643494d032f9c7e882d447e2fba5b73a0f770c83
Daniel6996-arch <nyutu.daniel@student.moringaschool.com>
2022-01-27 00:51:35 +0300

    edit and style dashboard

--- src/app/staff-dashboard/staff-dashboard.component.ts
+++ src/app/staff-dashboard/staff-dashboard.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+
+
 @Component({
   selector: 'app-staff-dashboard',
   templateUrl: './staff-dashboard.component.html',
@@ -7,9 +9,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StaffDashboardComponent implements OnInit {
 
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  displayStyle = "none";
+
+  openPopup() {
+    this.displayStyle = "block";
+  }
+
+  closePopup() {
+    this.displayStyle = "none";
+  }
+  
+  displatyle = "none";
+ 
+  openPopu() {
+    this.displatyle = "block";
+  }
+  
+  closePopu() {
+    this.displatyle = "none";
+  }
+
+  dispstyle = "none";
+ 
+  openPop() {
+    this.dispstyle = "block";
+  }
+  
+  closePop() {
+    this.dispstyle = "none";
   }
 
 }
