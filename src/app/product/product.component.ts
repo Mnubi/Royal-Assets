e928be56a556e951db85f840f981519abb763156
mnubi <rodger.kamau@student.moringaschool.com>
2022-01-28 03:55:51 +0300

    added route to assets

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Asset } from './../requests';
 
 @Component({
   selector: 'app-product',
@@ -7,9 +8,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductComponent implements OnInit {
 
+  assets: Asset[] = [
+    new Asset('Laptop', 'Electrical', 1225, 600,60),
+    new Asset('Printers', 'Electrical', 4, 1,1),
+    new Asset('Office Desk', 'Furniture', 120, 20,4),
+    
+  ];
+  addNewAsset(asset: any){
+    let assetLength = this.assets.length;
+    asset.id = assetLength+1;
+    this.assets.push(asset)
+  }
+
+  
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  
+  displatyle = "none";
+ 
+  openPopu() {
+    this.displatyle = "block";
+  }
+  
+  closePopu() {
+    this.displatyle = "none";
+  }
+
+  dispstyle = "none";
+ 
+  openPop() {
+    this.dispstyle = "block";
+  }
+  
+  closePop() {
+    this.dispstyle = "none";
+  }
+  
 
 }
