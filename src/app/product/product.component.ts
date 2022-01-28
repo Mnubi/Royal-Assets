fb18f9fff1ddecda4a89410e825e1ffb2cc7097c
mnubi <rodger.kamau@student.moringaschool.com>
2022-01-28 04:25:47 +0300

    styled form to add assets

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -9,9 +9,11 @@ import { Asset } from './../requests';
 export class ProductComponent implements OnInit {
 
   assets: Asset[] = [
-    new Asset('Laptop', 'Electrical', 1225, 600,60),
+    new Asset('Laptops', 'Electrical', 1225, 600,60),
     new Asset('Printers', 'Electrical', 4, 1,1),
-    new Asset('Office Desk', 'Furniture', 120, 20,4),
+    new Asset('Office Desks', 'Furniture', 120, 20,4),
+    new Asset('Office Chairs', 'Furniture', 120, 20,10),
+    new Asset('Projectors', 'Electrical', 5, 1,1),
     
   ];
   addNewAsset(asset: any){
