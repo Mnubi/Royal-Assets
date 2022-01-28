382ce5e8108f40123137fc95db0ccc287a800764
mnubi <rodger.kamau@student.moringaschool.com>
2022-01-28 03:56:25 +0300

    created link from manager dshboard

--- src/app/requests.ts
+++ src/app/requests.ts
@@ -7,3 +7,8 @@ export class Employee {
    constructor(public Name: string,public Department : string,public Contact : number,public Role : string){
    }
 }
+
+export class Asset {
+   constructor(public Item: string,public Category: string,public Assigned: number,public Stored : number,public Broken : number){
+   }
+}
