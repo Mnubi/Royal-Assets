214ac011186c0032952c6d2acf73a45f02d84b54
mnubi <rodger.kamau@student.moringaschool.com>
2022-01-29 19:08:24 +0300

    created path for asset form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,7 @@ import { AdminComponent } from './admin/admin.component';
 import { RequestFormComponent } from './request-form/request-form.component';
 import { AddStaffComponent } from './add-staff/add-staff.component';
 import { StaffComponent } from './staff/staff.component';
+import { AssetFormComponent } from './asset-form/asset-form.component';
 
 
 @NgModule({
@@ -38,6 +39,7 @@ import { StaffComponent } from './staff/staff.component';
     RequestFormComponent,
     AddStaffComponent,
     StaffComponent,
+    AssetFormComponent,
   ],
   imports: [
     BrowserModule,
