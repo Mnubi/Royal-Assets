9f3acfe8bd233053bd004aa7c5a198321164fe49
mnubi <rodger.kamau@student.moringaschool.com>
2022-01-31 01:52:49 +0300

    modified login page

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,28 +7,11 @@ import { FormBuilder, FormControl, FormGroup, Validators  } from '@angular/forms
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  myForm: any;
-
-  constructor(private formBuilder: FormBuilder) { }
-
-  ngOnInit(): void {
-    this.myForm = new FormGroup({
-      email: new FormControl(null, [
-        Validators.required,
-        Validators.email,
-        Validators.minLength(6)
-      ]),
-      password: new FormControl(null, [
-        Validators.required,
-        Validators.minLength(5),
-      ]),
-    });
-  }
-
-  
-  Submit(): void {
-    // console.log('Valid?', form.valid); // true or false
-    console.log(this.myForm.getRawValue());
-    // console.log('passord', myForm.value.message);
+  email : string = '';
+  password : string = '';
+  constructor() { }
+  ngOnInit(): void {}
+  login(){
+    console.log(this.email + " " +this.password)
   }
 }
