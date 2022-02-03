3e41c0e555f2cd594e21bdb1111eb8632bae7b10
mnubi <rodger.kamau@student.moringaschool.com>
2022-02-03 12:23:52 +0300

    introduced navbar to different dashboard

---
+++ src/app/navlog/navlog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navlog',
+  templateUrl: './navlog.component.html',
+  styleUrls: ['./navlog.component.css']
+})
+export class NavlogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
